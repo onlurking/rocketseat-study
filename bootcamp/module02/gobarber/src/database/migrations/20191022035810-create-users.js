ffa0da6798d4840cad95ed5ac133b9c29cf1584f
Diogo Felix <diogo2fex@gmail.com>
2019-10-22 01:22:48 -0300

    feat: first commig

---
+++ bootcamp/module02/gobarber/src/database/migrations/20191022035810-create-users.js
@@ -0,0 +1,44 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('users', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true
+      },
+      name: {
+        type: Sequelize.STRING,
+        required: true,
+        allowNull: false
+      },
+      email: {
+        type: Sequelize.STRING,
+        required: true,
+        allowNull: false,
+        unique: true
+      },
+      password_hash: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      provider: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+        defaultValue: false
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false
+      }
+    });
+  },
+
+  down: queryInterface => {
+    return queryInterface.dropTable('users');
+  }
+};
