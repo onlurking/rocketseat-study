f8c9091dcdf87e7f3a9d855807dadee765eb93ac
Diogo Felix <diogo2fex@gmail.com>
2019-10-23 20:34:06 -0300

    feat: add sequelize to gobarber

---
+++ bootcamp/module02/gobarber/src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: 'a1d94035-45a6-4da1-bdf9-8f391942bb79',
+  expiresIn: '7d'
+};
--- bootcamp/module02/gobarber/src/routes.js
+++ bootcamp/module02/gobarber/src/routes.js
@@ -1,9 +1,14 @@
 import { Router } from 'express';
 
+import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
+
+import authMiddleware from './app/middlewares/auth';
+
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'hello world' });
-});
+routes.post('/users', UserController.store);
+routes.post('/sessions', SessionController.store);
+routes.put('/users', authMiddleware, UserController.update);
 
 export default routes;
