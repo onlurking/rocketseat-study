64d6e168226fd11b9103150d198ab9a678215d4b
Diogo Felix <diogo2fex@gmail.com>
2019-10-24 01:42:17 -0300

    feat: add schedule route

--- bootcamp/module02/gobarber/src/routes.js
+++ bootcamp/module02/gobarber/src/routes.js
@@ -1,14 +1,16 @@
 import { Router } from 'express';
+
 import multer from 'multer';
 import multerConfig from './config/multer';
 
-import UserController from './app/controllers/UserController';
-import SessionController from './app/controllers/SessionController';
+import authMiddleware from './app/middlewares/auth';
+
+import AppointmentController from './app/controllers/AppointmentController';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
-import AppointmentController from './app/controllers/AppointmentController';
-
-import authMiddleware from './app/middlewares/auth';
+import ScheduleController from './app/controllers/ScheduleController';
+import SessionController from './app/controllers/SessionController';
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -22,6 +24,8 @@ routes.put('/users', UserController.update);
 routes.get('/providers', ProviderController.index);
 
 routes.post('/files', upload.single('file'), FileController.store);
+
+routes.get('/schedule', ScheduleController.index);
 
 routes.get('/appointments', AppointmentController.index);
 routes.post('/appointments', AppointmentController.store);
