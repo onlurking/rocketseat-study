8ada26319ae2d25525c6e58e64c5eb3bc5deff46
Diogo Felix <diogo2fex@gmail.com>
2019-10-24 00:54:55 -0300

    feat: add appointments

--- bootcamp/module02/gobarber/src/routes.js
+++ bootcamp/module02/gobarber/src/routes.js
@@ -6,6 +6,7 @@ import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
+import AppointmentController from './app/controllers/AppointmentController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -13,12 +14,13 @@ const routes = new Router();
 const upload = multer(multerConfig);
 
 routes.post('/users', UserController.store);
-
 routes.post('/sessions', SessionController.store);
-routes.put('/users', authMiddleware, UserController.update);
 
+routes.use(authMiddleware);
+
+routes.put('/users', UserController.update);
 routes.get('/providers', ProviderController.index);
-
 routes.post('/files', upload.single('file'), FileController.store);
+routes.post('/appointments', AppointmentController.store);
 
 export default routes;
