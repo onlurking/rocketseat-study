c18c097e460e97c07439b28250e7bc5d8de32f01
Diogo Felix <diogo2fex@gmail.com>
2019-10-24 01:08:21 -0300

    feat: add validations to appointment

--- bootcamp/module02/gobarber/src/app/controllers/AppointmentController.js
+++ bootcamp/module02/gobarber/src/app/controllers/AppointmentController.js
@@ -1,4 +1,5 @@
 import * as Yup from 'yup';
+import { startOfHour, parseISO, isBefore } from 'date-fns';
 import Appointment from '../models/Appointment';
 import User from '../models/User';
 
@@ -26,10 +27,28 @@ class AppointmentController {
       return res.status(401).json({ error: 'You can only create appointments with providers' });
     }
 
+    const hourStart = startOfHour(parseISO(date));
+
+    if (isBefore(hourStart, new Date())) {
+      return res.status(403).json({ error: 'Past date not permitted' });
+    }
+
+    const isNotAvailable = await Appointment.findOne({
+      where: {
+        provider_id,
+        canceled_at: null,
+        date: hourStart
+      }
+    });
+
+    if (isNotAvailable) {
+      return res.status(400).json({ error: 'Appointment date is not available' });
+    }
+
     const appointment = await Appointment.create({
       user_id: req.userId,
       provider_id,
-      date
+      date: hourStart
     });
 
     return res.json(appointment);
