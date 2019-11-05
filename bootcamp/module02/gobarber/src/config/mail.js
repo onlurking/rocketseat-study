f93378d4a661bf812086d17d8f4e783497073182
Diogo Felix <diogo2fex@gmail.com>
2019-11-05 00:00:46 -0300

    feat: add mailer

---
+++ bootcamp/module02/gobarber/src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: '',
+    pass: ''
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>'
+  }
+};
